8c137a9946c1ceb46d3f1a490148e4bd5a164589
vini23hot <vini23hot@gmail.com>
2018-11-22 12:24:20 -0200

    Added documentation and formatted the code

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # rubiks-cube-calculator
 
-> Simple calculator that shows the number of possible combinations of the Rubik's Cube for a friend
+> Minimalist calculator that shows the number of possible combinations of the Rubik's Cube
+
+**rubiks-cube-calculator** shows the possible combinations depending of the Rubik's Cube size. It was developed by me for a friend using the following libraries:
+
+- [Skeleton CSS Boilerplate](http://getskeleton.com/)
+- [bignumber.js](https://github.com/MikeMcl/bignumber.js/)
+- [Factorials](https://github.com/sankage/factorials)
+
+[**Demo available here!**](http://rubiks-cube-calculator.surge.sh/)
+
+![Preview Image](preview.png)
+
+## How is it calculated?
+
+The possible combinations is calculated using the Rubik's Cube size typed in the formula below:
+
+![Formula](formula.jpg)
+
+**n = Rubik's Cube size**
--- js/main.js
+++ js/main.js
@@ -1,67 +1,50 @@
-(function() {
+(function () {
   function addInputsEvent() {
-    const $inputs = document.querySelectorAll("#cube-input-1, #cube-input-2");
+    const $inputs = document.querySelectorAll('#cube-input-1, #cube-input-2');
+    addInputListener($inputs[0]);
+    addInputListener($inputs[1]);
+  }
 
-    $inputs[0].addEventListener(
-      "keyup",
-      function() {
-        $inputs[1].value = this.value;
-      },
-      false
-    );
-
-    $inputs[1].addEventListener(
-      "keyup",
-      function() {
-        $inputs[0].value = this.value;
-      },
-      false
-    );
+  function addInputListener(inputIndex) {
+    $inputs[inputIndex].addEventListener('keyup', function () {
+      $inputs[inputIndex === 0 ? 1 : 0].value = this.value;
+    }, false);
   }
 
   function addFormEvent() {
-    const $form = document.querySelector("form");
-    const $resultInput = document.querySelector("#cube-result");
+    const $form = document.querySelector('form');
+    const $resultInput = document.querySelector('#cube-result');
 
     $form.addEventListener(
-      "submit",
-      function(e) {
+      'submit', function (e) {
         e.preventDefault();
         const data = new FormData(this);
-        $resultInput.value = calculateCombinations(data.get("cube-input-1"));
-      },
-      false
-    );
+        $resultInput.value = calculateCombinations(data.get('cube-input-1'));
+      }, false);
   }
 
   function calculateCombinations(cubeSize) {
-    const firstPartTop = new BigNumber("24")
+    const firstPartTop = new BigNumber('24')
       .multipliedBy(new BigNumber(factorial(12)))
-      .multipliedBy(new BigNumber("2").pow(new BigNumber("10")))
+      .multipliedBy(new BigNumber('2').pow(new BigNumber('10')))
       .pow(new BigNumber(cubeSize).mod(new BigNumber(2)));
 
     const secondPartTopPow = new BigNumber(cubeSize)
-      .pow(new BigNumber("2"))
-      .minus(new BigNumber("2").multipliedBy(new BigNumber(cubeSize)))
-      .minus(
-        new BigNumber("3").multipliedBy(
-          new BigNumber(cubeSize).mod(new BigNumber("2"))
-        )
-      )
+      .pow(new BigNumber('2'))
+      .minus(new BigNumber('2').multipliedBy(new BigNumber(cubeSize)))
+      .minus(new BigNumber('3').multipliedBy(new BigNumber(cubeSize).mod(new BigNumber('2'))))
       .div(new BigNumber(4));
 
     const secondPartTop = new BigNumber(factorial(7))
-      .multipliedBy(new BigNumber("3").pow(new BigNumber("6")))
+      .multipliedBy(new BigNumber('3').pow(new BigNumber('6')))
       .multipliedBy(new BigNumber(factorial(24)).pow(secondPartTopPow));
 
     const firstPartBottom = new BigNumber(factorial(4)).pow(
-      new BigNumber("6").multipliedBy(
-        new BigNumber(cubeSize)
-          .minus(new BigNumber("2"))
-          .pow(new BigNumber("2"))
-          .minus(new BigNumber(cubeSize).mod(new BigNumber("2")))
-          .div(new BigNumber("4"))
-      )
+      new BigNumber('6').multipliedBy(new BigNumber(cubeSize)
+        .minus(new BigNumber('2'))
+        .pow(new BigNumber('2'))
+        .minus(new BigNumber(cubeSize).mod(new BigNumber('2')))
+        .div(new BigNumber('4')))
     );
 
     return new BigNumber(firstPartTop)
