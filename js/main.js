6290facf2bdbb2451292777cff8cb64b0859fdd5
viniciusmeneses <vini23hot@gmail.com>
2018-11-24 19:14:05 -0200

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@
   function addInputListener(input) {
     input.onkeyup = duplicateValue;
     input.onchange = duplicateValue;
+    console.dir(input)
   }
 
   function duplicateValue() {
+    if (this.value <= 0) {
+      this.value = 1
+    }
     $inputs[Array.from($inputs).indexOf(this) === 0 ? 1 : 0].value = this.value;
   }
 
